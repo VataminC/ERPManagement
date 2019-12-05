daac0a5cdcad0d951d18f34e3cdc105c495c126c
VitaminC <1920469233@qq.com>
2019-12-05 17:08:16 +0800

    commit

--- src/main/java/com/alliswell/controller/LoginController.java
+++ src/main/java/com/alliswell/controller/LoginController.java
@@ -1,6 +1,5 @@
 package com.alliswell.controller;
 
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.alliswell.pojo.User;
 import com.alliswell.service.UserService;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.net.ssl.HttpsURLConnection;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.HashMap;
